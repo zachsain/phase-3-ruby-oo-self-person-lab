40435147df17d5ea0c35576d6705d04a17e5b5ab
Zachary Sain <zachsain@gmail.com>
2022-03-31 14:21:52 -0500

    problems

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
-# your code goes here
+require "pry"
+
+class Person
+    attr_writer :bank_account
+    attr_writer :name
+    attr_writer :happiness
+    attr_writer :hygiene
+
+    def initialize(name)
+        @name = name
+    end 
+
+    def name(name)
+        name
+    end 
+
+    def bank_account(bank_account = 25)
+        @bank_account = bank_account
+    end 
+
+    def happiness(happiness = 8)
+        @happiness = happiness
+    end 
+
+    def hygiene(hygiene = 8)
+        @hygiene = hygiene
+    end 
+  
+end 
+
+linda = Person.new("Linda")
+
